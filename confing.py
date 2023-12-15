6d9f769f469556ddf0176cd346abee25943376d4
sa3ed7asan <127165039+sa3ed7asan@users.noreply.github.com>
2023-12-16 00:19:27 +0200

    Update confing.py

--- confing.py
+++ confing.py
@@ -7,3 +7,8 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
+OWNER_ID = int(getenv("OWNER_ID", "5451878368"))
+OWNER_NAME = getenv("OWNER_NAME", "ᯓ 𓆩 ˹𝙱𝙴𝙽˼ 𓆪 #1")
+
+SUBSCRIBE_CHANNEL = getenv(SUBSCRIBE_CHANNEL, "BENfiles")
+
