470f331c364133d62fd822ea9bcde40db0ccfb10
sa3ed7asan <127165039+sa3ed7asan@users.noreply.github.com>
2023-12-16 00:40:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = Client(
     "SessionsGenerator",
     api_id=config.API_ID,
     api_hash=config.API_HASH,
-    bot_token="config.BOT_TOKEN
+    bot_token=config.BOT_TOKEN
 )
 listener = Listener(client=app)
 
